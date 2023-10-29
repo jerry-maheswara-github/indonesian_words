ea742f8d71ad4bb99fb143aa29660e263c789aa5
Jerry Maheswara <jerrymaheswara@gmail.com>
2023-10-29 20:07:53 +0700

    Create indonesian_words.dart
    for education purpose

---
+++ lib/indonesian_words.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2017, filiph. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+// Moded by Jerry Maheswara fot educational purpose.
+
+/// Support for working with Indonesian text.
+library english_words;
+
+export 'src/syllables.dart';
+export 'src/word_pair.dart';
+export 'src/words/adjectives.dart';
+export 'src/words/all.dart';
+export 'src/words/nouns.dart';
